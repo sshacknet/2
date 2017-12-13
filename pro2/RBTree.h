abece95ee1d3728ef519fa777f010f0a47d21ea0
cyj <chenyijie16@126.com>
2017-12-14 00:34:18 +0800

    完成平衡二叉树

---
+++ pro2/RBTree.h
@@ -0,0 +1,126 @@
+﻿#pragma once
+
+#include <memory>
+
+typedef enum { RED, BLACK } Color;
+
+template <class KeyType>
+class RBTreeNode
+{
+public:
+    std::shared_ptr<RBTreeNode<KeyType>> right, left;//子节点
+    std::shared_ptr<RBTreeNode<KeyType>> p;//父节点
+    Color color;
+    KeyType key;
+
+    RBTreeNode(KeyType key): right(nullptr), left(nullptr), color(BLACK), key(key) { }
+};
+
+template <class KeyType>
+class RBTree
+{
+public:
+    std::shared_ptr<RBTreeNode<KeyType>> root;
+    std::shared_ptr<RBTreeNode<KeyType>> nil;
+    RBTree() : root(nullptr), nil(std::make_shared<RBTreeNode<KeyType>>(NULL)) { }
+
+    void insert(KeyType key);
+    bool serach(KeyType key);
+private:
+    void adjust(std::shared_ptr<RBTreeNode<KeyType>> z);
+    void left_rotate(std::shared_ptr<RBTreeNode<KeyType>> x);
+    void right_rotate(std::shared_ptr<RBTreeNode<KeyType>> x);
+};
+
+template <class KeyType>
+void RBTree<KeyType>::insert(KeyType key)
+{
+    auto z = std::make_shared<RBTreeNode<KeyType>>(key);
+    auto y = nil;
+    auto x = root;
+    while (x != nil)
+    {
+        y = x;
+        if (z->key < x.key)
+            x = x->left;
+        else
+            x = x->right;
+    }
+    z->p = y;
+    if (y == nullptr) { root = z; }
+    else if (z->key < y->key)
+    {
+        y->left = z;
+    }
+    else
+    {
+        y->right = z;
+    }
+
+    z.color = RED;
+    adjust(z);
+}
+
+template <class KeyType>
+void RBTree<KeyType>::adjust(std::shared_ptr<RBTreeNode<KeyType>> z)
+{
+    std::shared_ptr<RBTreeNode<KeyType>> y;
+    while (z->p->color = RED)
+    {
+        if (z->p == z->p->p->left)
+        {
+            y = z->p->p->right;
+            if (y->color == RED)
+            {
+                z->p->color = BLACK;
+                y->color = BLACK;
+                z->p->p->color = RED;
+                z = z->p->p;
+            }
+            else if (z == z->p->right)
+            {
+                z = z->p;
+                left_rotate(z);
+            }
+            z->p->color = BLACK;
+            z->p->p->color = RED;
+            right_rotate(z->p->p);
+        }
+        else
+        {
+            ;
+        }
+    }
+}
+
+template <class KeyType>
+void RBTree<KeyType>::left_rotate(std::shared_ptr<RBTreeNode<KeyType>> x)
+{
+    auto y = x->right;
+    x->right = y->left;
+    if (y->left != nil)
+    {
+        y->left->p = x;
+    }
+    y->p = x->p;
+    if (x->p == nil)
+    {
+        root = y;
+    }
+    else if (x == x->p->left)
+    {
+        x->p->left = y;
+    }
+    else
+    {
+        x->p->right = y;
+    }
+    y->left = x;
+    x->p = y;
+}
+
+template <class KeyType>
+void RBTree<KeyType>::right_rotate(std::shared_ptr<RBTreeNode<KeyType>> x)
+{
+    //
+}
--- pro2/main.cpp
+++ pro2/main.cpp
@@ -1,8 +1,53 @@
 #include <QtCore/QCoreApplication>
+#include <thread>
+#include "AVLTree.hpp"
+#include <iostream>
+#include <fstream>
+#include "CharString.h"
+#include <codecvt>
+extern const std::locale empty_locale = std::locale::empty();
+typedef std::codecvt_utf8<wchar_t> converter_type; 
+extern const converter_type* converter = new converter_type;
+extern const std::locale utf8_locale = std::locale(empty_locale, converter);
 
+
+
+using namespace std;
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
+    //return a.exec();
+    AVLTree<String> tree;
 
-    return a.exec();
+    std::wifstream dic_fin("dictionary/dictionary.dic");
+    dic_fin.imbue(utf8_locale);
+    int total = 0;
+    if (dic_fin)
+    {
+        wstring temp;
+        while (getline(dic_fin, temp))
+        {
+            total++;
+            if (total % 1000 == 0)
+            {
+                cout << "\r Loading Dictionary... [" << total << "] words";
+            }
+            String tmp(temp);
+
+            tree.insert(tmp);
+        }
+    }
+    else
+    {
+        cout << "error";
+        throw Error("fail to load dictionary");
+    }
+    cout << tree.search(String(L"天意")) << endl;
+    cout << tree.search(String(L"挖机")) << endl;
+    cout << tree.search(String(L"反季节")) << endl;
+    cout << tree.search(String(L"芙佳洞水库")) << endl;
+    cout << tree.search(String(L"方法")) << endl;
+    cout << tree.height();
+    system("pause");
+    return 0;
 }
